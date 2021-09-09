c5d022770e1dafb93febf4174bfa4f90bb11a1e6
ttruong100 <70781120+ttruong100@users.noreply.github.com>
2021-09-08 21:46:20 -0500

    Create 1.12Zylabs.py

---
+++ homework1/1.12Zylabs.py
@@ -0,0 +1,11 @@
+#Thuyen Truong
+#1780701
+
+
+#get an integer from input, and output that integer squared, ending with newline
+userNum = int(input())
+
+
+userNumSquared = userNum * userNum # Bug here; fix it when instructed
+
+print(userNumSquared, end='\n') # Output formatting issue here; fix it when instructed
