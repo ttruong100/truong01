a57ea60277798891b43e0806b81df72e1b27c0f9
ttruong100 <70781120+ttruong100@users.noreply.github.com>
2021-09-08 21:04:38 -0500

    Update CodingProblem1.py

--- homework1/CodingProblem1.py
+++ homework1/CodingProblem1.py
@@ -4,7 +4,7 @@
 #ask user for current date by month, day and year
 
 print("Birthday Calculator")
-print("Current day")
+print("Current Day")
 currentmonth = input("Month: ")
 currentday = int(input("Day: "))
 currentyear = int(input("Year: "))
@@ -16,18 +16,16 @@ birthdate = int(input("Day: "))
 birthyear = int(input("Year: "))
 
 #calculate birthday by year
-
 cal_birthday = currentyear - birthyear
 
 #user birthday haven't passed yet
-
-not_passed_birthmonth = cal_birthday - 1
+not_passed_birthday = cal_birthday - 1
 
 if currentday == birthdate and currentmonth == birthmonth:
     print("You are", cal_birthday, "years old.")
     print("Happy Birthday!")
 elif currentday <= birthdate and currentmonth == birthmonth:
-    print("You are", not_passed_birthmonth, "years old.")
+    print("You are", not_passed_birthday, "years old.")
 else:
     print("You are", cal_birthday, "years old." )
 
